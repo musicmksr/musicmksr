37f0e80dc7eff13eb3f9b08ec9c29bb6df754c66
ashermanwmf <shermanandrew50@gmail.com>
2016-11-05 12:14:56 -0700

    (refactor) Refactored back to original json addition

--- src/client/app/components/Profile.jsx
+++ src/client/app/components/Profile.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Track from './Track.jsx';
-import data from '../data.json';
-
-console.log(data.matrix);
 
 class Profile extends React.Component {
 
   render() {
     return(
-    	<div>
-	     	{data.matrix.map(track => 
-	     		<Track track={track} />
-	     	)}
-     	</div>
+    	<div>Profile</div>
     ); 
   };
 
