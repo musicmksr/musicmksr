8a38ed3c64887615ff8f9e3e37accafbb458ff53
Chris Doo <fivewingedangel@gmail.com>
2016-11-04 19:19:11 -0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports.keys = {
-  clientID: '1160256910722155', //FACEBOOK_APP_ID
-  clientSecret: '9eca1d21351fbf2929e952f9b1a08cb5', //FB_APP_SECRET,
-  callbackURL: 'http://localhost:3000/auth/facebook/callback'
-}
