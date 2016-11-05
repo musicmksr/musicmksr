e94832cf88a1757c44c65347575ec60f68880554
ashermanwmf <shermanandrew50@gmail.com>
2016-11-04 19:36:58 -0700

    (refactor) Uncached config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports.keys = {
-  clientID: '1160256910722155', //FACEBOOK_APP_ID
-  clientSecret: '0a30929f5cfa313fb36902a39a7400af', //FB_APP_SECRET,
-  callbackURL: 'http://localhost:3000/auth/facebook/callback'
-}
