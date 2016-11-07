7e403a4a4ad63cdea2c9036a88f75103597f0fcb
Alexander Ting <AlexTing@Alexanders-MacBook-Pro.local>
2016-11-07 11:28:01 -0800

    [feature] Added stepIndex to each sample and aggregated all steps in steps array.

--- src/client/app/components/Track.jsx
+++ src/client/app/components/Track.jsx
@@ -2,15 +2,33 @@ import React from 'react';
 import Howler from 'react-howler';
 import Sample from './Sample.jsx';
 
+let lastId = 0;
+let steps = [];
 class Track extends React.Component {
+
+  setStepIndex() {
+    if (lastId === 16){
+      lastId = 0;
+    }
+    lastId++;
+    return lastId;
+  }
+
   render() {
     return(
       <div>
-        {this.props.track.map((step, index) =>
-          <Sample step={step} data={this.props.data} index={this.props.index} sound= {new Howl({
-              src: `./samples/${this.props.sound}`
-          })}/>
-        )}
+        {
+          this.props.track.map((step, index) =>
+            {
+              let sample =
+              <Sample step={step} data={this.props.data} stepIndex={this.setStepIndex()} index={this.props.index} sound= {new Howl({
+                  src: `./samples/${this.props.sound}`
+              })}/>
+              steps.push(sample);
+              return sample;
+            }
+          )
+        }
       </div>
     )
   };
---
+++ src/client/app/components/rowCounter.js
@@ -0,0 +1,5 @@
+let lastId = 0;
+export default () => {
+  lastId++;
+  return lastId;
+};
