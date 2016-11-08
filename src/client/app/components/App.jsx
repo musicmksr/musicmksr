6cf36e74c6c9c93886b0248ae6686abf35b7b791
ashermanwmf <shermanandrew50@gmail.com>
2016-11-08 14:30:57 -0800

    (refactor) Added a boostrap menu

--- src/client/app/components/App.jsx
+++ src/client/app/components/App.jsx
@@ -6,16 +6,27 @@ class App extends React.Component {
 	render() {
 		return(
 			<div>
-				<h1>MusicMKS</h1>
-				<div role='nav'>
-					<Link to='/' onlyActiveOnIndex>Home</Link>
-					<Link to='/sequencer'>Sequencer</Link>
-					<Link to ='/profile'>Profile</Link>
-					<Link to ='/user'>User</Link>
-					<Link to ='/upload'>Upload</Link>
-					<Link to='/login'>Login</Link>
-					<Link to='/logout'>Logout</Link>
-				</div>
+				<nav className="navbar navbar-default">
+	        <div className="container-fluid">
+	          <div className="navbar-header">
+	            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	              <span className="sr-only">Toggle navigation</span>
+	              <span className="icon-bar"></span>
+	              <span className="icon-bar"></span>
+	              <span className="icon-bar"></span>
+	            </button>
+	            <Link className="navbar-brand" to='/sequencer'>Boot Fader</Link>
+	          </div>
+	          <div id="navbar" className="navbar-collapse collapse">
+	            <ul className="nav navbar-nav navbar-right">
+	              <li><Link to='/sequencer'>Sequencer</Link></li>
+	              <li><Link to ='/user'>User</Link></li>
+	              <li><Link to ='/upload'>Upload</Link></li>
+	              <li><Link to='/login'>Login</Link></li>
+	            </ul>
+	          </div>
+	        </div>
+	      </nav>
 				{this.props.children}
 			</div>
 		)
@@ -23,3 +34,14 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+// <div role='nav'>
+// 					<Link to='/' onlyActiveOnIndex>Home</Link>
+// 					<Link to='/sequencer'>Sequencer</Link>
+// 					<Link to ='/profile'>Profile</Link>
+// 					<Link to ='/user'>User</Link>
+// 					<Link to ='/upload'>Upload</Link>
+// 					<Link to='/login'>Login</Link>
+// 					<Link to='/logout'>Logout</Link>
+// 				</div>
