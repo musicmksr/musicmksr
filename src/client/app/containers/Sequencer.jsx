dd17aaa624dd63bd9b4857bd9b2e51777e8cbdaa
Alexander Ting <AlexTing@Alexanders-MacBook-Pro.local>
2016-11-08 16:09:42 -0800

    [feature] Added in redux skeleton for column change.

--- src/client/app/containers/Sequencer.jsx
+++ src/client/app/containers/Sequencer.jsx
@@ -4,7 +4,6 @@ import { bindActionCreators } from 'redux';
 import Track from '../components/Track.jsx';
 import toggleMatrix from '../actions/toggleMatrix.js';
 import setPlaySequence from '../actions/setPlaySequence';
-import data from '../data.json';
 
 let currentCol = 1;
 let innerPlay;
@@ -57,7 +56,7 @@ class Sequencer extends React.Component {
             	key={index}
             	track={track}
             	index={index}
-            	sound={data.samples[index]}
+            	sound={this.props.sequence.samples[index]}
               trackLength={this.props.sequence.matrix.length}
             	toggleMatrix={this.props.toggleMatrix.bind(this)}
             />
--- src/client/app/reducers/rootreducer.js
+++ src/client/app/reducers/rootreducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import matrixReducer from './reducer_playing';
 import playSequenceReducer from './reducer_playsequence';
+import columnReducer from './reducer_changecurrentcol';
 
 const rootReducer = combineReducers({
   routing: routerReducer,
   sequence: matrixReducer,
-  playSequence: playSequenceReducer
+  playSequence: playSequenceReducer,
+  changeCurrCol: columnReducer
 });
 
 export default rootReducer;
