2d24b9a71d54981654175c9dd17595a9fc0191d4
ashermanwmf <shermanandrew50@gmail.com>
2016-11-17 19:34:28 -0800

    (refactor) Fixed samples page for user profile

--- src/client/app/components/UserSamples.jsx
+++ src/client/app/components/UserSamples.jsx
@@ -3,25 +3,7 @@ import { render } from 'react-dom';
 import request from 'axios';
 
 class UserSamples extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      sound: ''
-    }
-  }
-  componentDidMount(){
-    this.getSongs();
-  }
-  getSongs() {
-    request.get(`/api/samples/${this.props.sample.name}.wav`)
-      .then((response) =>{
-        this.setState({
-          sound: response.data
-        });
-      });
-  }
   render() {
-    console.log(this.state.sound)
     return(
       <div className='col-md-3 userSample-container'>
         <div className='userSample'>
@@ -29,8 +11,8 @@ class UserSamples extends React.Component {
          <center><img src='imgs/sample.jpg' /></center>
           <audio controls>
             <source 
-              src={`/api/samples/${this.props.sample.name}.wav`} 
-              type={`audio/wav}`}>
+              src={`/api/sample/${this.props.sample.name}.wav`} 
+              type={`audio/wav`}>
             </source>
           </audio>
         </div>
@@ -39,5 +21,9 @@ class UserSamples extends React.Component {
   }
 
 }
+
+/*
+            
+*/
 
 export default UserSamples;
