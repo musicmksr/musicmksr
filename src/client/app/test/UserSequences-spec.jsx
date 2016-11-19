ed618a6b1a0401ccf8bb1d03f10e11926bc73f97
ashermanwmf <shermanandrew50@gmail.com>
2016-11-19 15:48:08 -0800

    [feature] Adding front end unit testing with enzyme

---
+++ src/client/app/test/UserSequences-spec.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import UserSequence from '../containers/UserSequence.jsx';
+
+describe('<UserSequence />', () =>{
+
+  it('should render without exploding', () =>{
+    const minProps = {
+      message: '',
+      messageCl: '',
+      bsStyle: ''
+      
+    };
+
+    expect(
+      shallow(
+        <UserSequence 
+          {...minProps} 
+        />
+      ).length
+    )
+    .to.equal(1);
+  });
+  
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -35,5 +35,11 @@ const config = {
   plugins: [
     new webpack.HotModuleReplacementPlugin()
   ],
+  externals: {
+    'cheerio': 'window',
+    'react/addons': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true
+  }
 };
 module.exports = config;
