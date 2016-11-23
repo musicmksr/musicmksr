9c1fb52e390148f512b0aaa62a0e70275f13da12
ashermanwmf <shermanandrew50@gmail.com>
2016-11-22 19:33:35 -0800

    [feature] Added loading routes

--- src/client/app/Routes.jsx
+++ src/client/app/Routes.jsx
@@ -8,9 +8,26 @@ import App from './components/App.jsx';
 import Profile from './components/Profile.jsx';
 import Sequencer from './containers/Sequencer.jsx';
 import rootReducer from './reducers/rootreducer.js';
+import request from 'axios';
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const history = syncHistoryWithStore(browserHistory, store);
+
+// if its a load link then we need to save info into local storage and redirect to sequence and load local storage beat
+let load = window.location.pathname.split('/');
+
+if(load.indexOf('load') >= 0){
+  request.get(`/api/sequence/${load[3]}/${load[2]}`)
+    .then((response) =>{
+      console.log(response);
+      window.localStorage.loadSequence = JSON.stringify(response.data);
+      window.location.href = '/';
+    })
+    .catch((err) =>{
+      console.log(err);
+      window.location.href = '/';
+    });
+}
 
 render((
   <Provider store={store}>
