031bc4f39ad1f403634f9b41478b1048a2be9a02
ashermanwmf <shermanandrew50@gmail.com>
2016-11-23 12:08:55 -0800

    [feature] share button to display sweet alert is working

--- spec/fronttest/App-spec.jsx
+++ spec/fronttest/App-spec.jsx
@@ -5,9 +5,26 @@ import App from '../../src/client/app/components/App.jsx';
 
 describe('<App />', () =>{
   it('should render without exploding', () =>{
+    const minProps = {
+      sequence: {
+        matrix: [[]],
+        samples: {},
+        name: ''
+      },
+      store: {
+        getState: () => {},
+        subscribe: () => {},
+        dispatch: () => {},
+        sequence: {
+          matrix: [[]],
+          samples: {},
+          name: ''
+        }
+      }
+    };
     expect(
       shallow(
-        <App />
+        <App {...minProps}/>
       ).length
     ).to.equal(1);
   });
